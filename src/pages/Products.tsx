f020ca80920223c6ad7085a2c8885a1350b87918
Azeem <Azeem@10XTEK.com>
2026-05-19 20:19:41 +0530

    catogery make 2 lines

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -36,7 +36,7 @@ const Products = () => {
         </p>
 
         {/* Category Filter - replaced with image cards */}
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8'>
           {[
             {
               name: 'SOAP',
